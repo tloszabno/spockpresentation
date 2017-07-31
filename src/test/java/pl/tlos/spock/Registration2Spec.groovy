4e8c380cfa7076fb6c86b68321483657e5786132
Tomasz Łoś <tloszabno@gmail.com>
2017-07-31 07:20:45 +0200

    More readadable tests

--- src/test/java/pl/tlos/spock/Registration2Spec.groovy
+++ src/test/java/pl/tlos/spock/Registration2Spec.groovy
@@ -2,39 +2,50 @@ package pl.tlos.spock
 
 import pl.tlos.browser.Browser
 import spock.lang.Specification
+import spock.lang.Title
+import spock.lang.Unroll
 
+@Title("Registration page specification")
 class Registration2Spec extends Specification{
     Browser browser = new Browser()
 
 
+    @Unroll
     def "Should not register when passwords not match"() {
         given:
-        "Registration page"()
+        'Registration page'()
+        def repeatedPassword = "1"
 
         when:
-        enter login: "valid", password: "123", repeated_password: "1"
+        entered login: "valid", password: "123", repeated_password: repeatedPassword
 
         and:
-        "submit clicked"()
+        'submit clicked'()
 
         then:
         'shown error'() == "Passwords do not match"
 
     }
 
-    def enter(params) {
+
+    def "Browser current page should be set"(){
+        expect:
+        browser.currentPage != null
+    }
+
+    def entered(params) {
         browser.currentPage.rightContent.elements.find({ it -> it.title == "login" }).enterText params.login
         browser.currentPage.rightContent.elements.find({ it -> it.title == "password" }).enterText params.password
         browser.currentPage.rightContent.elements.find({ it -> it.title == "passwordRepeat" }).enterText params.repeated_password
     }
-    def "submit clicked"() {
+    def 'submit clicked'() {
         browser.clickSubmit()
     }
     private String 'shown error'() {
         browser.currentPage.rightContent.error
     }
 
-    def "Registration page"() {
+    def 'Registration page'() {
         browser.goToRegistration()
     }
 }
